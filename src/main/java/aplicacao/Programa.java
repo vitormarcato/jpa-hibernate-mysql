e64d92d1dff66be2b631383552a3542663f86dd5
Vitor Marcato <vitor.marcatof@gmail.com>
2022-10-10 16:18:07 -0300

    data find

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -10,18 +10,15 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p2);
-		em.persist(p3);
-		em.persist(p1);
-		em.getTransaction().commit();
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
+		
 		System.out.println("concluído");
+		em.close();
+		emf.close();
 	}
 
 }
