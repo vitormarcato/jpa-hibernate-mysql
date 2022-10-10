5d8771ad58aa78aac372d1aa964ed99bfb40a425
Vitor Marcato <vitor.marcatof@gmail.com>
2022-10-10 16:25:43 -0300

    remove data

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -13,8 +13,15 @@ public class Programa {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
+		
+		//para remover um objeto instanciado no banco de dados ele precisa estar "monitorado", ou seja, o objeto acabou de ser inserido ou foi encontrado no banco de dados e o ainda não fechou o EntityManager.
+		
 		Pessoa p = em.find(Pessoa.class, 2);
-		System.out.println(p);
+		
+		//Além disso, transações que não são simples consulta devem ser feitas usando o getTransaction
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
 		
 		System.out.println("concluído");
 		em.close();
